e7d64d301f4aa58fb752be0657b688f040194420
Whiz7z <gjevstratov@gmail.com>
2023-04-14 19:30:24 +0100

    initialize client 5

--- src/redux/apis/productsApi.js
+++ src/redux/apis/productsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.BACKEND_URL}`,
+    baseUrl: process.env.BACKEND_URL,
     prepareHeaders: (headers, { getState }) => {
       const states = getState();
       if (states.user.userInfo && states.user.userInfo.token) {
